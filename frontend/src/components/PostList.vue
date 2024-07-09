873d07f810f9dd5e08ff84e6b3b5a5263fcfbc8c
Mengyan Wu <mengyanw@umich.edu>
2024-07-09 12:53:39 -0400

    Vue + Django + PostgreSQL

---
+++ frontend/src/components/PostList.vue
@@ -0,0 +1,36 @@
+<template>
+    <button @click="getPosts">Refresh</button>
+    <ul>
+        <li v-for="post in posts" :key="post.id" data-test="post">
+            {{ post.title }}
+        </li>
+    </ul>
+</template>
+
+<script>
+import axios from 'axios'
+console.log(import.meta.env.VITE_API_URL)
+
+export default {
+    data() {
+        return {
+            posts: null
+        }
+    },
+    watch: {
+        '$store.state.list': {
+            handler() {
+                this.getPosts();
+            },
+            immediate: true,
+        }
+    },
+    methods: {
+        async getPosts() {
+            const response = await axios.get(import.meta.env.VITE_API_URL + 'posts/')
+            this.posts = await response.data
+            console.log(response, this.posts)
+        }
+    }
+}
+</script>
